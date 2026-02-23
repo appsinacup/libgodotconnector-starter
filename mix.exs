6bde6efcf0b36521fd494c199df947efb9adfe8d
Dragos Daian <daian.dragos@yahoo.com>
2026-02-23 23:02:49 +0100

    set make instead of nmake

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule StarterApp.MixProject do
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:elixir_make] ++ Mix.compilers(),
+      make_env: [{"MAKE", "make"}]
     ]
   end
 
